dc95596248d64756105cfb838f301e11f0d73ce2
Igor Suvorov <hi@isuvorov.com>
2023-12-24 13:48:52 +0300

    feat(commands): add ycmd dev silent

--- libs/commands/src/dev.ts
+++ libs/commands/src/dev.ts
@@ -1,17 +1,25 @@
 // #!/usr/bin/env node
 import { createCommand, pnpmRecursive, shell } from 'ycmd';
 
+import { commonOptions } from './utils/commonOptions';
+
 export default createCommand({
   command: 'dev',
   describe: 'run development tasks',
 
+  builder: (yargs) =>
+    yargs.options({
+      prod: commonOptions.prod,
+      silent: commonOptions.silent,
+    }),
+
   // meta: import.meta,
-  async main({ isRoot, ctx }) {
+  async main({ argv, isRoot, ctx }) {
     if (isRoot) {
       await pnpmRecursive(`run dev`, { ctx, argv: { silent: true } });
       return;
     }
     // TODO: args to argv
-    await shell('ycmd build', { ctx, argv: { watch: true } });
+    await shell('ycmd build', { ctx, argv: { ...argv, watch: true } });
   },
 });
